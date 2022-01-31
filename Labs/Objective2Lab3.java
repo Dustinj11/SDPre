11c6ba709bcb11a0741c37dd7853a253b8362f0f
Dustin Jennings <92282089+Dustinj11@users.noreply.github.com>
2022-01-31 10:47:39 -0700

    Create Objective2Lab3.java

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,10 @@
+public class Objective2Lab3{
+  public static void main(String[] args){
+    double num1, num2, num3, average;
+    num1 = 32; 
+    num2 = 31; 
+    num3 = 4; 
+    averge = (num1 + num2 + num3) / 3; 
+    System.out.println(average); 
+  }
+}
